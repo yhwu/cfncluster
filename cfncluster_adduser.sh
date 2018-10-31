03ecf04218974ecd03feb31029aa8d48c3ba71ba
Yinghua Wu <yhwu@users.noreply.github.com>
2018-10-31 11:43:32 -0400

    Create cfncluster_adduser.sh

---
+++ cfncluster_adduser.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# following instructions from https://github.com/awslabs/cfncluster/issues/170
+#
+# by default, /shared, /home, /opt/sge are shared from master, so we only need to add users
+#
+# list user and userid in a file
+# awk -F: '/\/home/ {printf "%s,%s\n",$1,$3}' /etc/passwd > /home/ec2-user/cfnclusterusers.txt
+# chmod -w /home/ec2-user/cfnclusterusers.txt
+#
+# copy this file to S3 and make it public readable
+# aws s3 cp --acl public-read cfncluster_adduser.sh s3://xxx/cfncluster_adduser.sh
+#
+
+IFS=","
+while read USERNAME USERID
+     do
+     # -M do not create home since Master node is exporting /homes via NFS
+     # -u to set UID to match what is set on the Master node
+	 if ! [ `id -u $USERNAME 2>/dev/null || echo -1` -ge 0 ]; then 
+		useradd -M -u $USERID $USERNAME
+	fi	
+done < "/home/ec2-user/cfnclusterusers.txt"
