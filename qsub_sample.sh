c2f3e9e5d4d1dd344e336d18667375f06670adce
Yinghua Wu <yhwu@users.noreply.github.com>
2018-10-31 14:34:51 -0400

    Create qsub_sample.sh

---
+++ qsub_sample.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#$ -N test
+#$ -cwd
+#$ -V
+#$ -j yes
+#$ -pe smp 6
+#$ -R yes
+##$ -l h_vmem=15G
+##$ -l mem_free=14G
+#$ -t 1-4
+#$ -tc 20
+
+if [ -z "${SGE_TASK_ID}" ]; then
+    SGE_TASK_ID=1
+fi
+
+allinputs=($(seq -f '%04G' 0 100))
+myinput=${allinputs[$SGE_TASK_ID]}
+myhost=$(hostname)
+echo $SGE_TASK_ID $myhost $myinput
