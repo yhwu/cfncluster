0da41b1212c64c3d31e4b856de190338e9e6f7af
Yinghua Wu <yhwu@users.noreply.github.com>
2018-11-27 10:36:11 -0500

    Update qsub_sample.sh

--- qsub_sample.sh
+++ qsub_sample.sh
@@ -13,8 +13,8 @@
 if [ -z "${SGE_TASK_ID}" ]; then
     SGE_TASK_ID=1
 fi
-
+i=$(expr $SGE_TASK_ID - 1)
 allinputs=($(seq -f '%04G' 0 100))
 myinput=${allinputs[$SGE_TASK_ID]}
 myhost=$(hostname)
-echo $SGE_TASK_ID $myhost $myinput
+echo $SGE_TASK_ID $SGE_TASK_LAST $myhost $myinput
